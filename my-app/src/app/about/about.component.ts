c0e9f208aa3aa07da7619235f5d9d60830c00d52
EC2 Default User <ec2-user@ip-10-16-0-150.ec2.internal>
2022-12-01 17:01:55 +0000

    end of class

--- my-app/src/app/about/about.component.ts
+++ my-app/src/app/about/about.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+import { members } from '../team';
+
+
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
+
+  members = members;
+
+
 
 }
---
+++ my-app/src/app/team.ts
@@ -0,0 +1,16 @@
+export interface Member {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const members = [
+  {
+    id: 1,
+    name: 'Phone XL',
+    title: '',
+    bio: 'A large phone with one of the best screens',
+    photo_url: 'https://www.w3schools.com/w3images/team2.jpg'
+  }
+];
